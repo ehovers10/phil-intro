beac4e853bfa4d9f9449d31bcd964f88473b883b
ehovers10 <ehovers10@gmail.com>
2015-09-24 14:06:38 -0400

    initial commit

---
+++ analysis/index.md
@@ -0,0 +1,47 @@
+---
+layout: pres
+title: Analysis of Knowledge
+stitle: analysis
+date: 2015-09-27
+slides: 
+  [Meno, 
+  Justified True Belief, 
+  No False Lemmas, 
+  Fake Barns]
+---
+
+<div data-role="page" data-title="Meno" id="Meno" data-theme="e" class="eh-page">
+
+<div role="main" class="ui-content wrap">
+
+<h2 class="ui-bar ui-bar-a ui-corner-all">Meno</h2>
+
+</div>
+</div>
+
+<div data-role="page" data-title="{{ slides[1] | slugify }}" id="{{ slides[1] }}" data-theme="e" class="eh-page">
+
+<div role="main" class="ui-content wrap">
+
+<h2 class="ui-bar ui-bar-a ui-corner-all">Justified true belief</h2>
+
+</div>
+</div>
+
+<div data-role="page" data-title="{{ slides[2] | slugify }}" id="{{ slides[2] }}" data-theme="e" class="eh-page">
+
+<div role="main" class="ui-content wrap">
+
+<h2 class="ui-bar ui-bar-a ui-corner-all">No false lemmas</h2>
+
+</div>
+</div>
+
+<div data-role="page" data-title="{{ slides[3] | slugify }}" id="{{ slides[3] }}" data-theme="e" class="eh-page">
+
+<div role="main" class="ui-content wrap">
+
+# Fake barns
+
+</div>
+</div>
---
+++ js/msuphil100f15.js
@@ -0,0 +1,64 @@
+
+// Initiate externals
+
+$(function(){ 
+  $("[data-role=panel]").panel({ theme: "a" }).enhanceWithin();
+  $("[data-role=footer]").toolbar({ theme: "e" }).enhanceWithin();
+  $("[data-role=header]").toolbar({ theme: "e" }).enhanceWithin();
+});
+
+// Inject page-specific content in externals
+
+$( document ).on( "pagecontainerchange", function() {
+  
+  var current = $( ".ui-page-active" ).jqmData( "title" );
+
+  // Change heading
+  $( "[data-role='header'] h1" ).text( current );
+
+  // Change next and previous links
+  var next = "#" + $( ".ui-page-active" ).next( "[data-role='page']" ).attr("id"),
+      prev = "#" + $( ".ui-page-active" ).prev( "[data-role='page']" ).attr("id");
+
+  $( "#prev-link" ).attr("href", prev);
+  $( "#next-link" ).attr("href", next);
+
+  if ( $( ".ui-page-active" ).next( "[data-role='page']" ).length == 0 ) {
+      $( "#next-link" ).css( 'display','none' );
+  } else {
+      $( "#next-link" ).css( 'display','block' );
+  }
+  if ( $( ".ui-page-active" ).prev( "[data-role='page']" ).length == 0 ) {
+      $( "#prev-link" ).css( 'display','none' );
+  } else {
+      $( "#prev-link" ).css( 'display','block' );
+  }
+
+});
+
+$(document ).on( "swipeleft", function() {
+
+  var next = "#" + $( ".ui-page-active" ).next( "[data-role='page']" ).attr("id");
+
+  $( ":mobile-pagecontainer" ).pagecontainer( "change", next, {
+    transition: "slide",
+  });
+
+});
+
+$(document ).on( "swiperight", function() {
+
+  var prev = "#" + $( ".ui-page-active" ).prev( "[data-role='page']" ).attr("id");
+
+  $( ":mobile-pagecontainer" ).pagecontainer( "change", prev, {
+    transition: "slide",
+    reverse: true
+  });
+
+});
+
+  $( ".info" ).click( function() {
+
+    $(this).next( ".hid" ).slideToggle( "slow" );
+
+  }); 
